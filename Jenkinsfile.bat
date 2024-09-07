541f35f6b01c1df48fa60a05a72f34814f6a6f3a
Family <Family@host.docker.internal>
2024-09-07 22:42:14 +0200

    u

--- Jenkinsfile.bat
+++ Jenkinsfile.bat
@@ -11,60 +11,47 @@ pipeline {
         }
         stage('Clean up') {
    			 steps {
-				
-       			 //Remove old and unused Docker images from Docker
-       			 bat 'docker image prune --all --force'
-       			 
-       			 //Remove the existing container from Docker Hub
-		      	 bat 'docker stop charleshoanduong1111-job && docker rm -f charleshoanduong1111-job'
+       			 //Remove old and unused Docker images.  
+       			 bat 'docker image prune --all --force –'
        			 
   		 	 }
 		}        
         stage('Build Docker Image by Dockerfile') {
    			 steps {
-
+       			 //bat 'docker build -t charleshoanduong1111/angular-docker-image:$BUILD_NUMBER .'
+       			 //bat 'docker build -t charleshoanduong1111/jenkins:build . ' 
        			 bat 'docker build -t charleshoanduong1111/jenkins:build_%BUILD_NUMBER% .'
        			 
   		 	 }
 		}
 		stage('Login to Docker Hub') {
     		steps {
-				
-				bat 'echo DOCKERHUB_CREDENTIALS = %DOCKERHUB_CREDENTIALS% '
 				bat 'echo DOCKERHUB_CREDENTIALS_USR = %DOCKERHUB_CREDENTIALS_USR% '
+        		bat 'echo DOCKERHUB_CREDENTIALS = %DOCKERHUB_CREDENTIALS% '
         		bat 'echo DOCKERHUB_CREDENTIALS_PSW = %DOCKERHUB_CREDENTIALS_PSW%'
-        		
+        		//bat 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u ${DOCKERHUB_CREDENTIALS_USR} --password-stdin'
+        		//bat 'echo @CHChdChd11 | docker login -u charleshoanduong1111 --password-stdin'
+        		//bat 'docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"'
+        		//bat 'docker login -u=charleshoanduong1111 -p=@CHChdChd11'  //OK
+        		//bat 'docker login -u=charleshoanduong1111 -p=@CHChdChd11'  //OK
         		bat 'docker login -u=%DOCKERHUB_CREDENTIALS_USR%  -p=%DOCKERHUB_CREDENTIALS_PSW%'  //OK        		
     		}
 		}
-		stage('Push Image from Docker to Docker Hub for sharing to world users') {
+		stage('Push Image') {
     		steps {					
-
-        		bat 'docker push charleshoanduong1111/jenkins:build_%BUILD_NUMBER%'  
+       			//sh 'docker push **devopscloudbootcamp**/webapp:$BUILD_NUMBER'
+        		bat 'docker push charleshoanduong1111/jenkins:build_%BUILD_NUMBER%'  //OK
    			}
 		}	
-		stage('Deploy Application | Push Image from Docker to Docker Hub for sharing to world users') {
-    		steps {					
-
-        		bat 'docker push charleshoanduong1111/jenkins:build_%BUILD_NUMBER%'  
-   			}
-		}
-		stage('Run Angular App by running docker image') {
-    		steps {					
-
-        	    bat 'docker run -p 4200:4200 --name charleshoanduong1111-job charleshoanduong1111/jenkins:build_%BUILD_NUMBER%'
-
-   			}
-		}
 		stage('**Deploy Application**') {
    			 steps{
 		       bat 'echo TODO Deploy Application'
 		       
 		       // # remove the existing container
-		       //bat 'docker stop charleshoanduong1111-job && docker rm -f charleshoanduong1111-job'
+		       bat 'docker stop charleshoanduong1111-job && docker rm -f charleshoanduong1111-job'
 		       
 		       //# create and run a new container
-		       //bat 'docker run -p 4200:4200 --name charleshoanduong1111-job charleshoanduong1111/jenkins:build_%BUILD_NUMBER%'
+		       bat 'docker run -p 4200:4200 --name charleshoanduong1111-job charleshoanduong1111/jenkins:build_%BUILD_NUMBER%'
 		       //Ready! Next, we will access the URL http://localhost:4200/ 
 		       //and check if the application is working inside the Docker container.
 		       
@@ -83,6 +70,17 @@ pipeline {
 
 		     }
 		}
+		stage('**Remove Docker image from Docker**') {
+   			 steps{
+				//bat 'docker rmi $(docker images -q charleshoanduong1111/jenkins:*)'
+				//bat 'docker rmi %docker images -q charleshoanduong1111/jenkins:*%'
+				//bat "docker rmi -f $(docker images -f=reference='<image_name>:<tag_name>*'"
+				//bat "docker rmi $(docker images --filter=reference="nexus*/*/*:6.6.1-feature_1*" -q) -f"
+				bat 'echo Remove Docker image from Docker'
+				//TODO
+				//bat 'docker rmi $(docker images -filter=reference="charleshoanduong1111/jenkins:build*" -q) -f'
+		     }
+		}		
 		
     }
 }
